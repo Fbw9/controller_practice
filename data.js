09fb2370527448b103b739cc91f918e445be2bae
Ryan Kretch <rkretch91@gmail.com>
2019-01-15 16:26:57 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = {
+    restaurants: [
+        {
+           name: 'Hamy',
+           type: 'Vietnamese',
+           price: '$' 
+        }, 
+        {
+            name: 'Taco Bell',
+            type: 'Horrible Americanized Nasty Food',
+            price: '$' 
+        },
+        {
+            name: 'Little Tibets',
+            type: 'Tibetan',
+            price: '$$' 
+        }
+    ]
+}
